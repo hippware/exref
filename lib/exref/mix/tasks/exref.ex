ac64de5bd99c4fa744cc6d7d264dc3931781b17f
Phil Toland <phil.toland@gmail.com>
2016-09-19 14:48:03 -0500

    Elixirification of the exref module
    Mostly indentation, variable names and using pipelines for function chaining

--- lib/exref/mix/tasks/exref.ex
+++ lib/exref/mix/tasks/exref.ex
@@ -1,7 +1,14 @@
 defmodule Mix.Tasks.Exref do
   use Mix.Task
 
+  @moduledoc ""
+
   @shortdoc "Checks all function calls using xref"
+
+  @default_checks [
+    :locals_not_used, :exports_not_used,
+    :deprecated_function_calls, :deprecated_functions
+  ]
 
   def run(_args) do
     # make sure mix will let us run compile
@@ -15,15 +22,15 @@ defmodule Mix.Tasks.Exref do
     :xref.set_default(:xref, [{:verbose, false}])
     :xref.add_directory(:xref, String.to_char_list(ebin_dir))
 
-    r = xref_checks(default_checks, [])
+    r = xref_checks(@default_checks, [])
     display_results r
 
     if r != [], do: Mix.raise("xref found issues")
   end
 
-  ## ===================================================================
-  ## Internal functions
-  ## ===================================================================
+  # ===================================================================
+  # Internal functions
+  # ===================================================================
 
   defp ensure_compile do
     # we have to reenable compile and all of its
@@ -38,49 +45,46 @@ defmodule Mix.Tasks.Exref do
     |> Enum.filter(fn(t) -> match?("compile." <> _, t) end)
   end
 
-  defp default_checks do
-    [:locals_not_used, :exports_not_used,
-     :deprecated_function_calls, :deprecated_functions]
+  defp xref_checks(xref_checks, xref_ignores) do
+    run_xref_checks(xref_checks, xref_ignores, [])
   end
 
-  defp xref_checks(xrefChecks, xrefIgnores) do
-    run_xref_checks(xrefChecks, xrefIgnores, [])
-  end
-
-  defp run_xref_checks([], _xrefIgnores, acc) do
+  defp run_xref_checks([], _xref_ignores, acc) do
     acc
   end
-  defp run_xref_checks([xrefCheck | t], xrefIgnores, acc) do
-    {:ok, results} = :xref.analyze(:xref, xrefCheck)
-    case filter_xref_results(xrefCheck, xrefIgnores, results) do
-        [] ->
-            run_xref_checks(t, xrefIgnores, acc)
-        filterResult ->
-            run_xref_checks(t, xrefIgnores, [{xrefCheck, filterResult} | acc])
+  defp run_xref_checks([xref_check | t], xref_ignores, acc) do
+    {:ok, results} = :xref.analyze(:xref, xref_check)
+    case filter_xref_results(xref_check, xref_ignores, results) do
+      [] ->
+        run_xref_checks(t, xref_ignores, acc)
+      filter_result ->
+        run_xref_checks(t, xref_ignores, [{xref_check, filter_result} | acc])
     end
   end
 
-  ## Ignore behaviour functions, and explicitly marked functions
-  ##
-  ## Functions can be ignored by using
-  ## -ignore_xref([{F, A}, {M, F, A}...]).
-  defp get_xref_ignorelist(mod, xrefCheck) do
-    ## Get ignore_xref attribute and combine them in one list
+  # Ignore behaviour functions, and explicitly marked functions
+  #
+  # Functions can be ignored by using
+  # -ignore_xref([{F, A}, {M, F, A}...]).
+  defp get_xref_ignorelist(mod, xref_check) do
+    # Get ignore_xref attribute and combine them in one list
     attributes =
-        try do
-            mod.module_info(:attributes)
-        rescue
-            _ -> []
-        end
+      try do
+        mod.module_info(:attributes)
+      rescue
+        _ -> []
+      end
 
-    ignoreXref = keyall(:ignore_xref, attributes)
+    ignore_xref = keyall(:ignore_xref, attributes)
 
-    behaviourCallbacks = get_behaviour_callbacks(xrefCheck, attributes)
+    behaviour_callbacks = get_behaviour_callbacks(xref_check, attributes)
 
-    ## And create a flat {M,F,A} list
-    List.foldl(List.flatten([ignoreXref, behaviourCallbacks]), [],
-      fn ({f, a}, acc) -> [{mod,f,a} | acc]
-         ({m, f, a}, acc) -> [{m,f,a} | acc]
+    # And create a flat {M, F, A} list
+    [ignore_xref, behaviour_callbacks]
+    |> List.flatten
+    |> List.foldl([],
+      fn ({f, a}, acc) -> [{mod, f, a} | acc]
+         ({m, f, a}, acc) -> [{m, f, a} | acc]
       end)
   end
 
@@ -94,68 +98,71 @@ defmodule Mix.Tasks.Exref do
   defp get_behaviour_callbacks(:exports_not_used, attributes) do
     for b <- keyall(:behaviour, attributes), do: b.behaviour_info(:callbacks)
   end
-  defp get_behaviour_callbacks(_xrefCheck, _attributes) do
+  defp get_behaviour_callbacks(_xref_check, _attributes) do
     []
   end
 
   defp parse_xref_result({_, mfat}), do: mfat
   defp parse_xref_result(mfat), do: mfat
 
-  defp filter_xref_results(xrefCheck, xrefIgnores, xrefResults) do
-    searchModules = Enum.uniq(
-                      Enum.map(xrefResults,
-                        fn ({mt,_ft,_at}) -> mt
-                           ({{ms,_fs,_as},{_mt,_ft,_at}}) -> ms
-                           (_) -> nil
-                        end))
+  defp filter_xref_results(xref_check, xref_ignores, xref_results) do
+    search_modules =
+      xref_results
+      |> Enum.map(fn ({mt, _ft, _at}) -> mt
+                     ({{ms, _fs, _as}, {_mt, _ft, _at}}) -> ms
+                     (_) -> nil
+                  end)
+      |> Enum.uniq
 
     ignores =
-      xrefIgnores ++
-      Enum.flat_map(searchModules, &get_xref_ignorelist(&1, xrefCheck))
+      search_modules
+      |> Enum.flat_map(&get_xref_ignorelist(&1, xref_check))
+      |> Enum.concat(xref_ignores)
 
-    Enum.filter(xrefResults,
-                fn(r) -> not Enum.member?(ignores, parse_xref_result(r))
-                end)
+    Enum.filter(xref_results,
+                fn (r) -> not Enum.member?(ignores, parse_xref_result(r)) end)
   end
 
-  defp display_results(xrefResults) do
-    Enum.map(xrefResults, &display_xref_results_for_type/1)
+  defp display_results(xref_results) do
+    Enum.map(xref_results, &display_xref_results_for_type/1)
   end
 
-  defp display_xref_results_for_type({type, xrefResults}) do
-    Enum.map(xrefResults, &display_xref_result_fun(type, &1))
+  defp display_xref_results_for_type({type, xref_results}) do
+    Enum.map(xref_results, &display_xref_result_fun(type, &1))
   end
 
-  defp display_xref_result_fun(type, xrefResult) do
-    {source, sMFA, tMFA} =
-      case xrefResult do
-        {mFASource, mFATarget} ->
-          {format_mfa_source(mFASource),
-           format_mfa(mFASource),
-           format_mfa(mFATarget)}
-        mFATarget ->
-          {format_mfa_source(mFATarget),
-           format_mfa(mFATarget),
+  defp display_xref_result_fun(type, xref_result) do
+    {source, s_mfa, t_mfa} =
+      case xref_result do
+        {mfa_source, mfa_target} ->
+          {format_mfa_source(mfa_source),
+           format_mfa(mfa_source),
+           format_mfa(mfa_target)}
+        mfa_target ->
+          {format_mfa_source(mfa_target),
+           format_mfa(mfa_target),
            :undefined}
       end
+
     str =
       case type do
         :undefined_function_calls ->
-          "#{source}Warning: #{sMFA} calls undefined function #{tMFA} (Xref)\n"
+          "#{source}Warning: #{s_mfa} calls undefined function #{t_mfa}"
         :undefined_functions ->
-          "#{source}Warning: #{sMFA} is undefined function (Xref)\n"
+          "#{source}Warning: #{s_mfa} is undefined function"
         :locals_not_used ->
-          "#{source}Warning: #{sMFA} is unused local function (Xref)\n"
+          "#{source}Warning: #{s_mfa} is unused local function"
         :exports_not_used ->
-          "#{source}Warning: #{sMFA} is unused export (Xref)\n"
+          "#{source}Warning: #{s_mfa} is unused export"
         :deprecated_function_calls ->
-          "#{source}Warning: #{sMFA} calls deprecated function #{tMFA} (Xref)\n"
+          "#{source}Warning: #{s_mfa} calls deprecated function #{t_mfa}"
         :deprecated_functions ->
-          "#{source}Warning: #{sMFA} is deprecated function (Xref)\n"
+          "#{source}Warning: #{s_mfa} is deprecated function"
         other ->
-          "#{source}Warning: #{sMFA} - #{tMFA} xref check: #{other} (Xref)\n"
+          "#{source}Warning: #{s_mfa} - #{t_mfa} xref check: #{other}"
       end
-    IO.puts str
+
+    Mix.shell.info str
   end
 
   defp format_mfa({m, f, a}) do
@@ -164,15 +171,15 @@ defmodule Mix.Tasks.Exref do
 
   defp format_mfa_source(mfa) do
     case find_mfa_source(mfa) do
-        {:module_not_found, :function_not_found} -> ""
-        {source, :function_not_found} -> "#{source}: "
-        {source, line} -> "#{source}:#{line}: "
+      {:module_not_found, :function_not_found} -> ""
+      {source, :function_not_found} -> "#{source}: "
+      {source, line} -> "#{source}:#{line}: "
     end
   end
 
-  ##
-  ## Extract an element from a tuple, or undefined if N > tuple size
-  ##
+  #
+  # Extract an element from a tuple, or undefined if N > tuple size
+  #
   defp safe_element(n, tuple) do
     try do
       elem(n, tuple)
@@ -182,25 +189,25 @@ defmodule Mix.Tasks.Exref do
     end
   end
 
-    ##
-    ## Given a MFA, find the file and LOC where it's defined. Note that
-    ## xref doesn't work if there is no abstract_code, so we can avoid
-    ## being too paranoid here.
-    ##
+  #
+  # Given a MFA, find the file and LOC where it's defined. Note that
+  # xref doesn't work if there is no abstract_code, so we can avoid
+  # being too paranoid here.
+  #
   defp find_mfa_source({m, f, a}) do
     case :code.get_object_code(m) do
-        :error -> {:module_not_found, :function_not_found}
-        {m, bin, _} -> find_function_source(m,f,a,bin)
+      :error -> {:module_not_found, :function_not_found}
+      {m, bin, _} -> find_function_source(m, f, a, bin)
     end
   end
 
   defp find_function_source(m, f, a, bin) do
-    abstractCode = :beam_lib.chunks(bin, [:abstract_code])
-    {:ok, {^m, [{:abstract_code, {:raw_abstract_v1, code}}]}} = abstractCode
-    ## Extract the original source filename from the abstract code
+    abstract_code = :beam_lib.chunks(bin, [:abstract_code])
+    {:ok, {^m, [{:abstract_code, {:raw_abstract_v1, code}}]}} = abstract_code
+    # Extract the original source filename from the abstract code
     {:attribute, 1, :file, {source, _}} = List.keyfind(code, :file, 2)
     string_souce = List.to_string(source)
-    ## Extract the line number for a given function def
+    # Extract the line number for a given function def
     fun = Enum.filter(code,
                       fn(e) ->
                         safe_element(1, e) == :function and
@@ -208,11 +215,11 @@ defmodule Mix.Tasks.Exref do
                         safe_element(4, e) == a
                       end)
     case fun do
-        [{:function, line, ^f, _, _}] -> {string_souce, line}
-        ## do not crash if functions are exported, even though they
-        ## are not in the source.
-        ## parameterized modules add new/1 and instance/1 for example.
-        [] -> {string_souce, :function_not_found}
+      [{:function, line, ^f, _, _}] -> {string_souce, line}
+      # do not crash if functions are exported, even though they
+      # are not in the source.
+      # parameterized modules add new/1 and instance/1 for example.
+      [] -> {string_souce, :function_not_found}
     end
   end
 end
