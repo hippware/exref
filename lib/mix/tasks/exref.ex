aae767d73bfc0beb30654e1a06cc168d752920fa
Phil Toland <phil.toland@gmail.com>
2017-01-09 14:21:52 -0600

    Resolve Elixir 1.4 warnings

--- lib/mix/tasks/exref.ex
+++ lib/mix/tasks/exref.ex
@@ -17,7 +17,7 @@ defmodule Mix.Tasks.Exref do
 
   def run(_args) do
     # make sure mix will let us run compile
-    ensure_compile
+    ensure_compile()
     Mix.Task.run "compile"
 
     project = Mix.Project.config
@@ -41,7 +41,7 @@ defmodule Mix.Tasks.Exref do
     # we have to reenable compile and all of its
     # child tasks (compile.erlang, compile.elixir, etc)
     Mix.Task.reenable("compile")
-    Enum.each(compilers, &Mix.Task.reenable/1)
+    Enum.each(compilers(), &Mix.Task.reenable/1)
   end
 
   defp compilers do
--- mix.exs
+++ mix.exs
@@ -10,9 +10,9 @@ defmodule Exref.Mixfile do
       elixir:          "~> 1.2",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps:            deps,
-      description:     description,
-      package:         package,
+      deps:            deps(),
+      description:     description(),
+      package:         package(),
       source_url:      @github_url,
       homepage_url:    @github_url,
     ]
